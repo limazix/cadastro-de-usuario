78470f20d861d21ec96769b7d24643fa12d17e44
Bruno Cardoso <blimacardoso@fb.com>
2023-06-23 08:43:32 -0300

    Adding missing Item variable at list feature

--- src/features/list/List.js
+++ src/features/list/List.js
@@ -7,13 +7,13 @@ import Box from '@mui/material/Box';
 
 import { users as usersAPI } from "../../api";
 
-// const Item = styled(Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-// }));
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const List = () => {
     return (
